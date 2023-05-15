93b3c459485a2070601f2bd0727a3c40d7cb9edf
zeekblitz <trinityjosh89@gmail.com>
2023-05-14 20:44:43 -0400

    mostly done with UI

--- Assets/Scripts/JScripts/UI_Controller.cs
+++ Assets/Scripts/JScripts/UI_Controller.cs
@@ -7,26 +7,25 @@ using UnityEngine.UI;
 
 public class UI_Controller : MonoBehaviour
 {
-    //public GameObject staminaBar;
+    public Player player;
     public Image staminaBar;
     public GameObject heart;
     public GameObject heartContainer;
-    public Player player;
-    
 
     private PlayerStamina playerStamina;
     private int playerHealth;
     private List<Image> hearts;
-        
 
     // Start is called before the first frame update
     void Start()
     {
         playerStamina = player.GetComponent<PlayerStamina>();
+
         playerHealth = player.GetHealth();
-        for (int i = 0; i < playerHealth; i++){
+        for (int i = 0; i < playerHealth; i++)
+        {
             GameObject h = Instantiate(heart, heartContainer.transform);
-            hearts.Add(h.GetComponent<Image>());
+            //hearts.Add(h.GetComponent<Image>());
         }
     }
 
@@ -35,5 +34,18 @@ public class UI_Controller : MonoBehaviour
     {
         // update the stamina bar
         staminaBar.fillAmount = playerStamina.GetStamina() / 100f;
+
+        // update the number of hearts
+        if (playerHealth != player.GetHealth()){
+            while (player.GetHealth() < playerHealth){ // remove hearts
+                int temp = heartContainer.transform.childCount - 1;
+                Destroy(heartContainer.transform.GetChild(temp).gameObject);
+                playerHealth--;
+            }
+            while (player.GetHealth() > playerHealth){ // add hearts
+                GameObject h = Instantiate(heart, heartContainer.transform);
+                playerHealth++;
+            }
+        }
     }
 }
