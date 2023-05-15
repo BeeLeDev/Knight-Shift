e3cac80986042ed5a9a1919571f6d7618a191110
oopsuwu <60373320+oopsuwu@users.noreply.github.com>
2023-05-14 23:44:28 -0400

    Small Updates

--- Assets/Scripts/BScripts/BossController.cs
+++ Assets/Scripts/BScripts/BossController.cs
@@ -4,25 +4,11 @@ using UnityEngine;
 
 public class BossController : MonoBehaviour
 {
-    public AudioClip[] audioClips;
-
-    private AudioSource audioSource;
+  
     public GameObject spinHitbox;
     public GameObject explosionHitbox;
     public GameObject specialHitbox;
-
     private GameObject existingHitbox;
-
-    private void Start()
-    {
-        audioSource = GetComponent<AudioSource>();
-
-        if (audioClips.Length > 0)
-        {
-            // set the first audio clip to play
-            audioSource.clip = audioClips[0];
-        }
-    }
 
     private void CreateSpinHitbox()
     {
